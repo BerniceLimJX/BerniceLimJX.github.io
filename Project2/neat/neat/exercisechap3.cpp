5693d635751fbff07d583b75e0edd33d8f2ec85d
BerniceLimJX <bernicelimjingxuan@gmail.com>
2023-05-16 12:41:46 +0800

    upload project 2

---
+++ Project2/neat/neat/exercisechap3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string theString = "data";
+    string str = "engineering"
+    cout << theString <<endl;
+    cout << str << endl;
+
+   
+    cout <<  theString.append(str) <<endl;
+    cout << str << endl;
+
+    system("pause");
+    return 0;
+}
